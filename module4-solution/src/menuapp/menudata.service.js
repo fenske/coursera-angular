504039fe324e3607ad9d88f51b184cc970a501cc
Anton Fenske <anton.fenske@klarna.com>
2016-10-30 15:55:48 +0100

    Defined a service

---
+++ module4-solution/src/menuapp/menudata.service.js
@@ -0,0 +1,28 @@
+(function () {
+'use strict';
+
+angular.module('MenuDataService')
+.service('data', data);
+
+MenuDataService.$inject = ['$http']
+function MenuDataService($http) {
+  var service = this;
+
+  service.getAllCategories = function() {
+    return $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/categories.json")
+    });
+    return response;
+  }
+
+  service.getItemsForCategory = function(categoryShortName) {
+    return $http({
+      method: "GET",
+      url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName)
+    });
+    return response;
+  }
+}
+
+})();
