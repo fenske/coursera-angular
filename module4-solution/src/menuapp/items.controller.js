bd33b2f490da865a595c2d4c3c50aa3f284613f1
Anton Fenske <anton.fenske@klarna.com>
2016-10-30 21:16:57 +0100

    Fixed module dependencies

--- module4-solution/src/menuapp/items.controller.js
+++ module4-solution/src/menuapp/items.controller.js
@@ -4,7 +4,6 @@
 angular.module('MenuApp')
 .controller('ItemsController', ItemsController);
 
-
 ItemsController.$inject = ['MenuDataService', 'items'];
 function ItemsController($stateParams, items) {
   var itemsCtrl = this;
